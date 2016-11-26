85ff0d4a6ec487f1818a642d525449b4c8709409
Chrisi Hendricks <hendricks.christina@gmail.com>
2016-11-26 18:49:41 -0500

    Style our history page

--- our_history_page.php
+++ our_history_page.php
@@ -7,12 +7,22 @@ Template Name: Our History Page
 <?php get_header(); ?>
     <div class="container">
       <section class="row">
+        <div class="twelve columns">
+          <h2>Meet Slick</h2>
+        </div>
+      </section>
+      <section class="row">
           <div class="six columns">
             <?php dynamic_sidebar('history1-widget'); ?>
           </div>
           <div class="six columns">
             <?php dynamic_sidebar('history2-widget'); ?>
           </div>
+      </section>
+      <section class="row">
+        <div class="twelve columns">
+          <h2>Our Employees</h2>
+        </div>
       </section>
       <section class="row">
           <div class="six columns">
