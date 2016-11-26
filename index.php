a33459bc49f8e266d97ff054ae004dc9bcab029b
Chrisi Hendricks <hendricks.christina@gmail.com>
2016-11-26 13:38:36 -0500

    Style blog area of index page

--- index.php
+++ index.php
@@ -89,15 +89,17 @@
                       } //end if
                         ?>
                 </div>
-                <div class="six columns">
+                <div class="six columns blog-link">
                   <?php
                     if ( have_posts() ) {
                       while ( have_posts() ) {
                         the_post(); ?>
 
-                      <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+                      <h3><?php the_title(); ?></h3>
+                      <span class="date"><?php echo get_the_date(); ?></span>
                       <?php the_excerpt(); ?>
                       <a href="<?php the_permalink(); ?>">Read More</a>
+                      <a href="http://www.chrisihendricks.net/wie-final2016/blog/">Archive</a>
 
                   <?php
                     } //end while
